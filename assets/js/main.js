d26986dbddc251cd4e918cfeef319fbf4b841627
Kevin Souto <kevinsoutods@gmail.com>
2022-07-13 16:57:20 -0300

    Inserindo os ID

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
-let precoPago = 6;
-let quantProdTotal = 1000;
+let precoIngrediente = 6;
+let quantProdutoTotal = 1000;
 let quantReceita = 360;
 let custoIngredinte = 0;
 
- const calculoCusto = (precoPago, quantProdTotal, quantReceita) => {
-    return ( (precoPago / quantProdTotal) * quantReceita );
+ const calculoCusto = (precoIngrediente, quantProdutoTotal, quantReceita) => {
+    return ( (precoIngrediente / quantProdutoTotal) * quantReceita );
  }
 
- custoIngredinte = (calculoCusto(precoPago, quantProdTotal, quantReceita));
+ custoIngredinte = (calculoCusto(precoIngrediente, quantProdutoTotal, quantReceita));
 
  //arredondando para duas casas decimais 
  console.log("R$ " + (custoIngredinte).toFixed(2) + " para cada " +  quantReceita + "g");
